2c84d393e42984e1ba9988b8959c630396ba4f6d
João Silva <joaosilvapedro357@gmail.com>
2023-11-16 10:03:30 +0000

    My fav page done, update

--- src/Pages/MyMemesPage/index.jsx
+++ src/Pages/MyMemesPage/index.jsx
@@ -1,82 +1,76 @@
-import { useState } from "react"
-import { Link, useParams, useNavigate } from "react-router-dom"
-import axios from "axios"
-import { useEffect } from "react"
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-
-const MEME_API = "https://api.memegen.link"
-
-const REST_API = "http://localhost:5005"
+const REST_API = "http://localhost:5005";
 
 function MyMemes() {
-
-  const [savedMemes, setSavedMemes] = useState();               // created memes on JSON file
+  const [savedMemes, setSavedMemes] = useState([]);
   const [isFavourite, setIsFavourite] = useState(false);
 
   const {id} = useParams();
+  const navigate = useNavigate();
 
-    // FECTH INITIAL DATA OF THE JSON!? --> MY MEMES PAGE 
+  useEffect(() => {
+    axios.get(`${REST_API}/created`).then((response) => {
+      const created = response.data;
+      setSavedMemes(created);
+    }).catch((error) => console.log(error));
+  }, []);
+
+  const handleToggle = (id) => {
+    const updatedMemes = savedMemes.map((meme) =>
+      meme.id === id ? { ...meme, favorite: !meme.favorite } : meme
+    );
+
+    // Move the favorite memes to the beginning of the array
+    const sortedMemes = [...updatedMemes].sort((a, b) => (b.favorite ? 1 : 0) - (a.favorite ? 1 : 0));
+
+    setSavedMemes(sortedMemes);
+
+    const updatedMeme = sortedMemes.find((meme) => meme.id === id);
     
-     useEffect(()=>{
-        axios.get(`${REST_API}/created`).then((response)=>{
-          const created = response.data;
-          setSavedMemes(created);
-          console.log(created)
-        })
-        .catch((error) => console.log(error));
-    }, []);
-  
-    // Favourite on/off
-    const handleToggle = () => {
-      setIsFavourite(!isFavourite);
-    }
+    // Update the favorite status in the API
+    axios.post(`${REST_API}/created/${id}`, { favorite: updatedMeme.favorite })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
 
-    /*const handleDelete = (savedMemesId) => {
-      const deletedMemes = savedMemes.filter(savedMemes => savedMemes.id !== savedMemesId);
-      setSavedMemes(deletedMemes);*/
-
-    const navigate = useNavigate(); 
-
-    const deleteMeme = (id) =>{
-      axios.delete(`${REST_API}/created/${id}`).then(()=>{
-        console.log("data deleted");
-          navigate("/mymemes");
-      })
-      .catch((error)=> console.log(error)); // Api não tem Delete (nem Put)
+  const deleteMeme = (id) => {
+    axios.delete(`${REST_API}/created/${id}`).then(() => {
+      const updatedMemes = savedMemes.filter((meme) => meme.id !== id);
+      setSavedMemes(updatedMemes);
+      navigate("/mymemes");
+    }).catch((error) => console.log(error));
   }
-
 
   return (
     <div className="mymemes-main">
-      {/*<h2 className="mymemes-header"> Created Memes </h2>*/}
       <div className="mymemes-column">
         <h2> My Memes </h2>
-        {savedMemes &&
-        savedMemes.map((meme, id)=>{
-          return(
-            <div key={id}>
-              <Link> <img className="saved-images" src={meme.imgLink} /> </Link>
-            </div>
-          )
-        })}
+        {savedMemes.map((meme) => (
+          <div key={meme.id}>
+            <Link>
+              <img className={`saved-images ${meme.favorite ? "favorite" : ""}`} src={meme.imgLink} />
+            </Link>
+          </div>
+        ))}
       </div>
-
-      <div className="new-meme"> 
-        {savedMemes && 
-        savedMemes.filter((meme)=> meme.id === savedMemes.length).map((filteredMeme)=>{
-          return(
-            <div key={filteredMeme.id}>
-              <img className="created-image" src={filteredMeme.imgLink} />
-              <div className="mymemes-button">
-              <button className="favourite-button" onClick={handleToggle}> {isFavourite? "❤️" : "♡"} </button>
+      <div className="new-meme">
+        {savedMemes.map((filteredMeme) => (
+          <div key={filteredMeme.id}>
+            <img className="created-image" src={filteredMeme.imgLink} />
+            <div className="mymemes-button">
+              <button className="favourite-button" onClick={() => handleToggle(filteredMeme.id)}>
+                {filteredMeme.favorite ? "❤️" : "♡"}
+              </button>
               <button className="delete-button" onClick={() => deleteMeme(filteredMeme.id)}> Delete </button>
-              </div>
             </div>
-          )})}
-        
+          </div>
+        ))}
       </div>
     </div>
   )
 }
 
-export default MyMemes
+export default MyMemes;
