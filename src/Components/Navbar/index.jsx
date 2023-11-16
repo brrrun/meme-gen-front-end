6ef04a91e72038500756bd455577f53bc32218bb
brrrun <brunomsantosmendes@gmail.com>
2023-11-16 15:13:47 +0000

    navbar update.2

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="homepage-link"><img className="corner-meme" src="./../../images\homepage icon-.png"/></Link>
+      <Link to="/" className="homepage-link"><img className="corner-meme" src="./../../images\homepage icon.png"/></Link>
         <div className="navbar-links">
-          <Link to="getpage" className="get-link"> CREATE NEW  </Link> 
+          <Link to="getpage" className="get-link"> NEW MEME </Link> 
           <p>|</p>
           <Link to="/mymemes" className="favourites-link"> MY MEMES</Link>
         </div>
